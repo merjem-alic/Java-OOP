33b9d57e3d840459e0a7069df5defc991f095c74
alicmerjem <merjem.alic@stu.ibu.edu.ba>
2025-02-03 23:14:05 +0100

    Some examples

---
+++ javatutorial/Rectangle.java
@@ -0,0 +1,13 @@
+public class Rectangle {
+    public static void main(String[] args) {
+        int length = 4;
+        int width = 6;
+        int area;
+
+        area = length * width;
+
+        System.out.println("Length comes out to: " + length);
+        System.out.println("Width comes out to: " + width);
+        System.out.println("Area of the rectangle is: " + area);
+    }
+}
---
+++ javatutorial/Students.java
@@ -0,0 +1,15 @@
+public class Students {
+    public static void main(String[] args) {
+        String studentName = "John Doe";
+        int studentID = 15;
+        int studentAge = 23; 
+        float studentFee = 75.25f;
+        char studentGrade = 'B';
+
+        System.out.println("Student name: " + studentName);
+        System.out.println("Student ID: " + studentID);
+        System.out.println("Student age: " + studentAge);
+        System.out.println("Student fee: " + studentFee);
+        System.out.println("Student grade: " + studentGrade);
+    }
+}
