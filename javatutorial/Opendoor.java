cfa5d13773402b01982c11f8b43d2f6118b1c85b
alicmerjem <merjem.alic@stu.ibu.edu.ba>
2025-02-05 21:38:53 +0100

    Example 1

---
+++ javatutorial/Opendoor.java
@@ -0,0 +1,11 @@
+public class Opendoor {
+    public static void main(String[] args) {
+        int doorCode = 1337;
+
+        if (doorCode == 1337) {
+            System.out.println("Correct code. Welcome home!");
+        } else {
+            System.out.println("Wrong code. Not so fast, weather boy.");
+        }
+    }
+}
