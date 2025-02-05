fb710eaf98ede2636c097c9dbcf4fc9ab05aabc5
alicmerjem <merjem.alic@stu.ibu.edu.ba>
2025-02-05 21:27:08 +0100

    If...else statement

---
+++ javatutorial/Voting.java
@@ -0,0 +1,12 @@
+public class Voting {
+    public static void main(String[] args) {
+        int myAge = 25;
+        int votingAge = 18;
+
+        if (myAge >= votingAge) {
+            System.out.println("You are old enought to vote!");
+        } else {
+            System.out.println("You are not old enought to vote!");
+        }
+    }
+}
