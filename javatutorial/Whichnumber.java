2989734bcb2f2c43c19b25f220ee20b8761116cf
alicmerjem <merjem.alic@stu.ibu.edu.ba>
2025-02-05 21:40:54 +0100

    Example 2

---
+++ javatutorial/Whichnumber.java
@@ -0,0 +1,13 @@
+public class Whichnumber {
+    public static void main(String[] args) {
+        int myNum = 10;
+
+        if (myNum > 0) {
+            System.out.println("The value is a positive number.");
+        } else if (myNum < 0) {
+            System.out.println("The value is a negative number.");
+        } else {
+            System.out.println("The value is zero.");
+        }
+    }
+}
