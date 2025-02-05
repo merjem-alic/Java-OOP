e5c71bee8d4ef159e843416ba80097d8eb72ff41
alicmerjem <merjem.alic@stu.ibu.edu.ba>
2025-02-05 21:45:00 +0100

    Example 3

---
+++ javatutorial/Evenodd.java
@@ -0,0 +1,11 @@
+public class Evenodd {
+    public static void main(String[] agrs) {
+        int num = 5;
+
+        if (num % 2 == 0) {
+            System.out.println("The given number is even.");
+        } else {
+            System.out.println("The given number is odd.");
+        }
+    }
+}
