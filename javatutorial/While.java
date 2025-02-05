0d45b3532ff7d9a273e1e098784d99da5f22b52a
alicmerjem <merjem.alic@stu.ibu.edu.ba>
2025-02-05 21:56:18 +0100

    While loop

---
+++ javatutorial/While.java
@@ -0,0 +1,12 @@
+public class While {
+    public static void main(String[] args) {
+        int i = 0;
+        
+        while (i < 5) {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
+
+// dont forget to add i++ at the end, unless you want an infinite loop
