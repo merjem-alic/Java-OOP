f601a2d186f87445c9d3231cf02490af1a444f40
alicmerjem <merjem.alic@stu.ibu.edu.ba>
2025-02-05 22:16:17 +0100

    For loop

---
+++ javatutorial/Forloop.java
@@ -0,0 +1,15 @@
+public class Forloop {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            System.out.println(i);
+        }
+
+    // another example, this will only print even values between 0 and 10
+
+    for (int i = 0; i <= 10; i = i + 2) {
+        System.out.print(i);
+    }
+}
+}
+
+// for loop in java is the same as for loop in cpp
