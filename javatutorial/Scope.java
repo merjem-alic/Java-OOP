5c43526f417987ab7336d6455d81e715504b36a4
alicmerjem <merjem.alic@stu.ibu.edu.ba>
2025-02-06 22:53:39 +0100

    Scope explained

---
+++ javatutorial/Scope.java
@@ -0,0 +1,18 @@
+public class Scope {
+    public static void main(String[] args) 
+    
+    // code here cannot use x
+
+    {  // this is a block
+
+        // code here cannot use x
+
+        int x = 100;
+
+        // code here can use x
+        System.out.println(x);
+
+    } // the block ends here
+
+    // code here cannot use x
+}
