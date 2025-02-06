9458310785d6901aaa1506656117c02fd7fcf529
alicmerjem <merjem.alic@stu.ibu.edu.ba>
2025-02-06 20:21:14 +0100

    Multiple parameters

---
+++ javatutorial/Parameters2.java
@@ -0,0 +1,11 @@
+public class Parameters2 {
+    static void myMethod(String fname, int age) {
+        System.out.println(fname + " is " + age);
+    }
+
+    public static void main(String[] agrs) {
+        myMethod("Liam", 5);
+        myMethod("Jenny", 8);
+        myMethod("Anja", 31);
+    }
+}
